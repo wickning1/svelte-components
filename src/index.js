7577acc3c46c262a7a4de35280101717f37be016
Nick Wing <git@mythgaming.net>
2019-10-05 13:59:56 -0500

    version upgrade

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 export { default as CardLayout } from './CardLayout.svelte'
 export { default as Card } from './Card.svelte'
+export { default as FlexCardLayout } from './FlexCardLayout.svelte'
+export { default as FlexCard } from './FlexCard.svelte'
