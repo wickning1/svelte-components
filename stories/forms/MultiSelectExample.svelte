a71d955c9ca011096fcc779582f99b5b26b7bc0d
Nick Wing <git@mythgaming.net>
2019-12-05 07:41:20 -0600

    better labels on the multiselect story

--- stories/forms/MultiSelectExample.svelte
+++ stories/forms/MultiSelectExample.svelte
@@ -2,9 +2,9 @@
   import { MultiSelect } from '../../src'
 </script>
 
-<MultiSelect name="test" label="Fruit" value={['apple']} getOptions={async val => ['apple', 'orange', 'banana'].filter(o => o.includes(val))} />
+<MultiSelect name="test" label="Fruit with only values" value={['apple']} getOptions={async val => ['apple', 'orange', 'banana'].filter(o => o.includes(val))} />
 
-<MultiSelect name="test" label="Fruit" value={['orange', 'banana']} placeholder='Choose some fruit' getOptions={async val => [
+<MultiSelect name="test" label="Fruit with value and label" value={['orange', 'banana']} placeholder='Choose some fruit' getOptions={async val => [
   { value: 'apple', label: 'Apple'},
   { value: 'orange', label: 'Orange' },
   { value: 'banana', label: 'Banana' }
